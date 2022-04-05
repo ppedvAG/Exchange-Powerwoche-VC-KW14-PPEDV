9a8b9930d39337023961ce2ee7f357334bd6201e
AlexanderSchwarzppedvAG <alexanders@ppedv.de>
2022-04-05 08:35:11 +0200

    Create PowerShellVirtualDirectory.ps1

---
+++ PowerShellVirtualDirectory.ps1
@@ -0,0 +1,5 @@
+﻿Add-PSSnapin *ex*
+#Virtuelles Verzeichnis "Powershell" neu erstellen
+Get-PowerShellVirtualDirectory -Server hv-ex19-des | Remove-PowerShellVirtualDirectory
+New-PowerShellVirtualDirectory -Server hv-ex19-des -Name Powershell
+Get-PowerShellVirtualDirectory -Server hv-ex19-des | Set-PowerShellVirtualDirectory -RequireSSL:$false
